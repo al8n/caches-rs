a48d8a3e00eaba37530c0a39b61b54aab5505f49
Al Liu <scygliu1@gmail.com>
2023-10-17 18:40:58 +0800

    Fix CI

--- ci/sanitizer.sh
+++ ci/sanitizer.sh
@@ -6,12 +6,12 @@ export ASAN_OPTIONS="detect_odr_violation=0 detect_leaks=0"
 
 # Run address sanitizer with cargo-hack
 RUSTFLAGS="-Z sanitizer=address" \
-cargo hack test --lib --each-feature
+cargo test --lib --each-feature
 
 # Run leak sanitizer with cargo-hack
 RUSTFLAGS="-Z sanitizer=leak" \
-cargo hack test --lib --each-feature
+cargo test --lib --each-feature
 
 # Run thread sanitizer with cargo-hack
 RUSTFLAGS="-Z sanitizer=thread" \
-cargo hack -Zbuild-std test --lib --each-feature
+cargo -Zbuild-std test --lib --each-feature
