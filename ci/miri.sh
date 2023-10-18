7f232839d8c964e77266df9aa6d0862db34651ca
Al Liu <scygliu1@gmail.com>
2023-10-18 15:15:09 +0800

    relax miri

--- ci/miri.sh
+++ ci/miri.sh
@@ -5,7 +5,7 @@ rustup toolchain install nightly --component miri
 rustup override set nightly
 cargo miri setup
 
-export MIRIFLAGS="-Zmiri-strict-provenance -Zmiri-disable-isolation -Zmiri-symbolic-alignment-check"
+export MIRIFLAGS="-Zmiri-disable-isolation -Zmiri-symbolic-alignment-check"
 
 cargo miri test
 
