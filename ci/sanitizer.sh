8048a572a3a60beca58664f515f1478fdb0e90c4
Al Liu <scygliu1@gmail.com>
2025-01-11 01:28:58 +0800

    Cleanup CI (#26)

--- ci/sanitizer.sh
+++ ci/sanitizer.sh
@@ -2,8 +2,21 @@
 
 set -ex
 
-export ASAN_OPTIONS="detect_odr_violation=0 detect_leaks=0"
+# export ASAN_OPTIONS="detect_odr_violation=0 detect_leaks=0"
 
-# Run leak sanitizer
-RUSTFLAGS="-Z sanitizer=leak" \
-cargo test --lib
+# Run address sanitizer
+RUSTFLAGS="--cfg all_skl_tests -Z sanitizer=address" \
+cargo test --lib --all-features --target x86_64-unknown-linux-gnu
+
+# # Run leak sanitizer
+# RUSTFLAGS="--cfg all_skl_tests -Zsanitizer=leak" \
+# cargo test -Zbuild-std --release --tests --target x86_64-unknown-linux-gnu --features memmap
+
+# Run memory sanitizer
+RUSTFLAGS="--cfg all_skl_tests -Zsanitizer=memory -Zsanitizer-memory-track-origins" \
+RUSTDOCFLAGS="-Zsanitizer=memory -Zsanitizer-memory-track-origins" \
+cargo test -Zbuild-std --release --tests --target x86_64-unknown-linux-gnu 
+
+# Run thread sanitizer
+RUSTFLAGS="--cfg all_skl_tests -Z sanitizer=thread" \
+cargo -Zbuild-std test --lib --target x86_64-unknown-linux-gnu
